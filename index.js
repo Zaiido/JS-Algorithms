5709942852cb9819f5d5cbdb027a2190eccf55c3
Zaide Kurti <zaiido1920@gmail.com>
2022-11-28 14:36:55 +0100

    Homework

---
+++ index.js
@@ -0,0 +1,11 @@
+let myFirstName = "Zaide";
+let myLastName = "Kurti";
+let myFullName = myFirstName + " " + myLastName;
+
+console.log("Hello, my name is", myFullName);
+
+let myAge = 23;
+let currentYear = 2022;
+let myBirthYear = currentYear - myAge;
+
+console.log("I was born in", myBirthYear);
